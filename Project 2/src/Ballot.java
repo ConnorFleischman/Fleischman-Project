0ddff57f68f33cbe3f02494ecdf1b40c260e8a21
ConnorFleischman <111784986+ConnorFleischman@users.noreply.github.com>
2023-05-14 21:00:48 -0400

    Comments for Project 2

--- Project 2/src/Ballot.java
+++ Project 2/src/Ballot.java
@@ -1,15 +1,40 @@
+/**
+ * This is the ballot class which creates a boolean list with length of 'numberCandidates'
+ */
 public class Ballot {
+
     private boolean[] candidates;
+
+    /**
+     * This sets the length of the list
+     * @param numberCandidates - the number of candidates which is specified by the user
+     */
 
     public Ballot(int numberCandidates){
         candidates = new boolean[numberCandidates];
     }
+
+    /**
+     * This sets the boolean value of the index position in the list to true
+     * @param index - the index is the position of a candidate in the list
+     */
     public void mark(int index) {
         candidates[index] = true;
     }
+
+    /**
+     * This returns the boolean value of the specified index
+     * @param index - the index is the position of a candidate in the list
+     * @return - this returns the T/F value of the index
+     */
     public boolean getMark(int index) {
         return candidates[index];
     }
+
+    /**
+     * This returns the length of the candidates list
+     * @return - this returns the length of the list as an int
+     */
     public int countCandidates() {
         return candidates.length;
     }
--- Project 2/src/Voter.java
+++ Project 2/src/Voter.java
@@ -1,5 +1,13 @@
 import java.util.Random;
+
+/**
+ * This hold the vote method which creates a new ballot and marks it on a random candidate
+ */
 public class Voter {
+    /**
+     * This allows the program to mark the ballot as True at a random position
+     * @param votie - an instance of the voting machine class
+     */
     public void vote(VotingMachine votie) {
         Ballot ballie = votie.getBallot();
         int numberCandidates = ballie.countCandidates();
